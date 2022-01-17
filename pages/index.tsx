38ca23f2ab395dd4575dd5f833a88f2c73f259f1
Jessej Samuel <1234jessej@gmail.com>
2022-01-17 14:50:32 +0530

    Check SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,8 +16,11 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <meta property="og:image" content="/assets/twttr_banner.png" />
-        <meta property="twitter:image" content="/assets/twtter_banner.png" />
+        {/* <meta property="og:image" content="/assets/652f40427e1f5186ad54836074898279.png" /> */}
+        <meta
+          property="twitter:image"
+          content="/assets/652f40427e1f5186ad54836074898279.png"
+        />
         <meta name="revisit-after" content="1 days" />
       </Head>
       <div
