d5b0927cc7424f4b40ea8a9d0418df5b3ff30ba3
Jessej Samuel <1234jessej@gmail.com>
2022-01-17 13:02:35 +0530

    Feat: UI improvement for dark mode

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
       <input
         title="Search"
         type="search"
-        className="bg-inherit border border-slate-400 w-full text-sm p-2 hover:shadow-md shadow-black rounded-full h-11 px-5 outline-none focus-visible:dark:bg-slate-700 hover:dark:bg-slate-700 transition sm:max-w-lg"
+        className="bg-inherit border border-slate-400 dark:border-slate-700 w-full text-sm p-2 hover:shadow-md shadow-black rounded-full h-11 px-5 outline-none focus-visible:dark:bg-slate-700 hover:dark:bg-slate-700 transition sm:max-w-lg"
         autoComplete="True"
         value={this.state.term}
         placeholder="🔍 Search Here"
