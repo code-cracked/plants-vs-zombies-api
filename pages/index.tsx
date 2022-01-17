4f92f62a04ba1326c4b243d5f21861dbe728680c
Jessej Samuel <1234jessej@gmail.com>
2022-01-17 12:03:52 +0530

    Add: Bounce animation to down arrow

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ const Home: NextPage = () => {
           <SearchBar />
         </div>
         <div className="bottom-4">
-          <a href="#about" className="dark:text-slate-400 ">
+          <a href="#about" className="dark:text-slate-400">
             <svg
               width="24"
               height="24"
@@ -42,6 +42,7 @@ const Home: NextPage = () => {
               fillRule="evenodd"
               clipRule="evenodd"
               fill="currentColor"
+              className="motion-safe:animate-bounce"
             >
               <path d="M12 0c6.623 0 12 5.377 12 12s-5.377 12-12 12-12-5.377-12-12 5.377-12 12-12zm0 1c6.071 0 11 4.929 11 11s-4.929 11-11 11-11-4.929-11-11 4.929-11 11-11zm5.247 8l-5.247 6.44-5.263-6.44-.737.678 6 7.322 6-7.335-.753-.665z" />
             </svg>
