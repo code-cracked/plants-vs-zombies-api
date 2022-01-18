c6a876a4f16667da63c84ce289065590606e4bbe
Dani-Hacker <danieljebarson21@gmail.com>
2022-01-19 00:06:44 +0530

    Basic docs till now

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 const Api: NextPage = () => {
   return (
     <>
@@ -40,8 +41,115 @@ const Api: NextPage = () => {
             </li>
           </ul>
         </div>
-        <div id="github_issues"></div>
-        <div></div>
+        <div id="plants">
+          <h1 className="font-bold text-2xl my-4">PLANTS-NAMES (endpoint)</h1>
+          <p>
+            Inorder to get the plants-names with the help of PVZ API use the
+            below http-address
+          </p>
+          <br></br>
+          <div className="flex box-border">
+            <h5>
+              https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/plants
+            </h5>
+            <CopyToClipboard text="https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/plants">
+              <button className="shadow-md   hover:shadow-md hover:animate-spin  h-6 w-6">
+                📋
+              </button>
+            </CopyToClipboard>
+          </div>
+          The above link returns a list of all plant's name in Plants vs Zombies
+        </div>
+        <div id="plant_info">
+          <h1 className="font-bold text-2xl my-4">
+            Plant Information (endpoint)
+          </h1>
+          <p>
+            Inorder to get the specific plant-information with the help of PVZ
+            API use the below http-address
+          </p>
+          <br></br>
+          <div className="flex box-border">
+            <h5>
+              https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/plants/[plant_name]
+            </h5>
+            <CopyToClipboard text="https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/plants/[plant_name]">
+              <button className="shadow-md   hover:shadow-md hover:animate-spin  h-6 w-6">
+                📋
+              </button>
+            </CopyToClipboard>
+          </div>
+          <br></br>
+          Replace{" "}
+          <b>
+            <i>plant_name</i>
+          </b>{" "}
+          with{" "}
+          <b>
+            <i>Plant to be searched</i>
+          </b>{" "}
+          . Then the above link returns a json file of information about the
+          specific plant .<br></br>
+          <pre>
+            Example:
+            https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/plants/Aloe
+          </pre>
+        </div>
+        <div id="areas">
+          <h1 className="font-bold text-2xl my-4">AREAS-NAMES (endpoint)</h1>
+          <p>
+            Inorder to get the areas-names with the help of PVZ API use the
+            below http-address
+          </p>
+          <br></br>
+          <div className="flex box-border">
+            <h5>
+              https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/areas
+            </h5>
+            <CopyToClipboard text="https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/areas">
+              <button className="shadow-md   hover:shadow-md hover:animate-spin  h-6 w-6">
+                📋
+              </button>
+            </CopyToClipboard>
+          </div>
+          <br></br>
+          The above link returns a list of all area's name in Plants vs Zombies
+        </div>
+        <div id="area_info">
+          <h1 className="font-bold text-2xl my-4">
+            Area Information (endpoint)
+          </h1>
+          <p>
+            Inorder to get the specific area-information with the help of PVZ
+            API use the below http-address
+          </p>
+          <br></br>
+          <div className="flex box-border">
+            <h5>
+              https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/areas/[area_name]
+            </h5>
+            <CopyToClipboard text="https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/areas/[area_name]">
+              <button className="shadow-md   hover:shadow-md hover:animate-spin  h-6 w-6">
+                📋
+              </button>
+            </CopyToClipboard>
+          </div>
+          <br></br>
+          Replace{" "}
+          <b>
+            <i>plant_name</i>
+          </b>{" "}
+          with{" "}
+          <b>
+            <i>Plant to be searched</i>
+          </b>{" "}
+          . Then the above link returns a json file of information about the
+          specific area .<br></br>
+          <pre>
+            Example:
+            https://pvz2-website-g0dr91qps-code-cracked.vercel.app/api/areas/Player's%20House
+          </pre>
+        </div>
       </div>
     </>
   );
