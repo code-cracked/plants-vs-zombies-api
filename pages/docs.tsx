c56812cf2d761b04a085ddac7ed799ee5d2fd5ef
Dani-Hacker <danieljebarson21@gmail.com>
2022-01-19 19:43:33 +0530

    changed links

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -219,7 +219,7 @@ const Docs: NextPage = () => {
                 <CopyToClipboard
                   text="https://pvz2-website.vercel.app/api/areas"
                   onCopy={(e) => {
-                    console.log("Copied!");
+                    // console.log("Copied!");
                     notify();
                   }}
                 >
